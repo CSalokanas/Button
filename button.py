b317446ef7c805b676fd4ae397c5d492836261b7
Christos <christossalokanas@gmail.com>
2025-03-02 18:29:01 +0100

    Working Button

--- button.py
+++ button.py
@@ -1,26 +1,32 @@
 import pygame
 
 class Buttons:
-    def __init__(self, text, screen, size_x=150, size_y=50, font="freesansbold.ttf", text_size=20,button_color ="gray",x_pos=0,y_pos=0, text_color="black"):
+    def __init__(self, text, screen, size_x=150, size_y=50, font="freesansbold.ttf", text_size=20,button_color ="gray",x_pos=0,y_pos=0, text_color="white"):
+        super().__init__()
+        self.button_map = {
+            "Size_X": size_x,
+            "Size_Y": size_y,
+            "Color": button_color,
+            "Text": text,
+            "Text_Size": text_size,
+            "Text_Color": text_color,
+            "Position_X": x_pos,
+            "Position_Y": y_pos,
+            "Font": font,
+            "screen": screen
+        }
         self.font = pygame.font.Font(font, text_size)
-        self.text = text
-        self.text = self.font.render(text, True, color=text_color)
-        self.x = x_pos
-        self.y = y_pos
-        self.screen = screen
-        self.size_x = size_x
-        self.size_y = size_y
-        self.button_color = button_color
+        self.text = self.font.render(text, True, text_color)
 
     def draw(self):
-        button_rect = pygame.rect.Rect(self.x, self.y, self.size_x, self.size_y)
-        pygame.draw.rect(surface=self.screen, color=self.button_color, rect=button_rect)
-        self.screen.blit(self.text, (self.x + 10, self.y + 10))
+        button_rect = pygame.rect.Rect(self.button_map["Position_X"], self.button_map["Position_Y"], self.button_map["Size_X"], self.button_map["Size_Y"])
+        pygame.draw.rect(surface=self.button_map["screen"], color=self.button_map["Color"], rect=button_rect)
+        self.button_map["screen"].blit(self.text, (self.button_map["Position_X"] + 10, self.button_map["Position_Y"] + 10))
 
     def check_click(self):
         mouse_pos = pygame.mouse.get_pos()
         if pygame.mouse.get_pressed()[0]:
-            if self.x < mouse_pos[0] < self.x + self.size_x:
-                if self.y < mouse_pos[1] < self.y + self.size_y:
+            if self.button_map["Position_X"] < mouse_pos[0] < self.button_map["Size_X"] + self.button_map["Position_X"]:
+                if self.button_map["Position_Y"] < mouse_pos[1] < self.button_map["Size_Y"] + self.button_map["Position_Y"]:
                     return True
         return False
