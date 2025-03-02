1a7bcebd4fc6bc887b4d52495c84c18e820475fa
Christos <christossalokanas@gmail.com>
2025-03-02 20:20:23 +0100

    just for fun

--- main.py
+++ main.py
@@ -5,18 +5,19 @@ pg.init()
 
 
 
-screen = pg.display.set_mode((1000, 600))
+screen = pg.display.set_mode((2000, 800))
 flag = True
 clock = pg.time.Clock()
 while flag:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             flag = False
-    button1 = Buttons("a",screen, x_pos=400, y_pos=300)
-    if button1.check_click():
+    button1 = Buttons("I hope you are having a blessed day",screen, x_pos=420, y_pos=420, borders=True, text_size=10)
+    button2 = Buttons("asdasda",screen, x_pos=420, y_pos=500, text_shadow=True, aliases=False)
+    if button1.check_click() or button2.check_click():
         flag = False
 
-
+    button2.draw()
     button1.draw()
     pg.display.update()
     clock.tick(60)
